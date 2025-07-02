27c5929b1afe5c4d09ddab04784e32e5ae94239b
Sooraj734056 <sumitsharma734056@gmail.com>
2025-07-02 12:25:37 +0530

    Initial commit for my portfolio

---
+++ script.js
@@ -0,0 +1,14 @@
+// Smooth scroll
+document.querySelectorAll('nav a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+// AOS init
+AOS.init({
+  duration: 1200,
+  once: true
+});
